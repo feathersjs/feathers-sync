90ba5f429d7025ef22d58b739525e65097233415
daffl <daff@neyeon.com>
2025-06-24 11:46:13 -0700

    Fix linting

---
+++ types/index.test-d.ts
@@ -0,0 +1,12 @@
+import { feathers } from "@feathersjs/feathers";
+import sync, { redis } from "feathers-sync";
+
+const app = feathers();
+
+app.configure(
+  sync({
+    uri: "redis://some/where",
+  }),
+);
+
+app.configure(redis({}));
